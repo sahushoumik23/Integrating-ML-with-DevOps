9c739bc89b30bcbb6796a177ee6c46837617b51f
Shoumik <sahushoumik23@gmail.com>
2020-05-26 20:02:22 +0530

    Update

--- Python_Files/append.py
+++ Python_Files/append.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import os
 
 acc=acc*100
@@ -7,13 +7,3 @@ if int(acc) > 80:
 	os.system('curl --user "admin:toor" http://192.168.1.105:8080/view/Task%203/job/success/build?token=success')
 else:
 	os.system('curl --user "admin:toor" http://192.168.1.105:8080/view/Task%203/job/Failure/build?token=Fail')
-=======
-import os 
-
-acc=acc*100
-
-if int(acc) >80 :
-	os.system(curl -u "admin:toor" 192.168.1.105:8080/view/Task%203/job/success/build?token=success)
-else:
-	os.system(curl -u "admin:toor" 192.168.1.105:8080/view/Task%203/job/Failure/build?token=Fail)
->>>>>>> 617c48b95847eb6d0e96aff04f089981487d2aef
